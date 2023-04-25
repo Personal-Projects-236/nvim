1b849cf890c1db6de51b2114142645e2a5ee4bb1
douglas <douglasmaxton@gmail.com>
2023-04-25 16:12:06 +0100

    Created shortcuts to easily switch between windon panes

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -21,3 +21,9 @@ mappings("n", "<leader>zt", "<cmd>Telekasten toggle_todo<CR>")
 
 -- Call insert link automatically when we start typing a link
 mappings("i", "[[", "<cmd>Telekasten insert_link<CR>")
+
+-- mapping to easily switch between window panes
+mappings("n", "<c-h>", "<c-w>h<CR>")
+mappings("n", "<c-j>", "<c-w>j<CR>")
+mappings("n", "<c-k>", "<c-w>k<CR>")
+mappings("n", "<c-l>", "<c-w>l<CR>")
