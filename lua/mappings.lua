20a46fbb54ffc14d22c6b5b1c6f135c84e071e4e
douglas <douglasmaxton@gmail.com>
2023-05-06 20:35:06 +0100

    Created a new shortcut for searching for all todo comments in code using telescope

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,6 +6,7 @@ mappings('n', 'Q', ':q<CR>') -- this will quit the terminal
 mappings('n', 'W', ':qall<CR>') -- this quits all terminals
 mappings('n', '<c-g>', ':FloatermNew --width=1.0 --height=1.0 --autoclose=2 lazygit<CR>')
 mappings('n', '<c-s>', ':COQsnips edit<CR>')
+mappings('n', 'T', ':TodoTelescope keywords=TODO,PERF,HACK,NOTE,FIX,WARN<CR>')
 
 -- Launch panel if nothing is typed after <leader>z
 mappings("n", "<leader>z", "<cmd>Telekasten panel<CR>")
