091654d88d9f58ca69b9f8a2a17541826ca49b92
douglas <douglasmaxton@gmail.com>
2024-02-23 10:52:18 +0000

    created one formatter command for all filetypes

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -22,9 +22,9 @@ packer.startup({function()
 		requires = "nvim-treesitter/nvim-treesitter"
 	})
 	use {
-   		'renerocksai/telekasten.nvim',
-   		requires = {'nvim-telescope/telescope.nvim', 'renerocksai/calendar-vim'}
-  }
+		'renerocksai/telekasten.nvim',
+		requires = {'nvim-telescope/telescope.nvim', 'renerocksai/calendar-vim'}
+	}
 	-- NOTE: completion engine
 	use { 'neoclide/coc.nvim', branch='release' }
 	-- NOTE: icons in neovim
@@ -32,86 +32,92 @@ packer.startup({function()
 	use("nvim-tree/nvim-web-devicons")
 	use('folke/tokyonight.nvim') -- Color Scheme
 	use {
-	  'nvim-lualine/lualine.nvim',
-	  requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+		'nvim-lualine/lualine.nvim',
+		requires = { 'nvim-tree/nvim-web-devicons', opt = true }
 	}
 	-- formater
 	use "vim-autoformat/vim-autoformat"
 	use({
 		"Pocco81/auto-save.nvim",
 		config = function()
-			 require("auto-save").setup {
+			require("auto-save").setup {
 				-- your config goes here
 				-- or just leave it empty :)
-		 	}
+			}
 		end,
 	})
-	use('voldikss/vim-floaterm') 
+	use('voldikss/vim-floaterm')
 	use {
-  		'lewis6991/gitsigns.nvim',
-  		config = function()
-    			require('gitsigns').setup()
-  		end
+		'lewis6991/gitsigns.nvim',
+		config = function()
+			require('gitsigns').setup()
+		end
 	}
 	use('nvim-lua/plenary.nvim')
 	use('kazhala/close-buffers.nvim')
 	-- use({
-	-- 	'prettier/vim-prettier',
-	-- 	pattern = {"*.js", "*.jsx","*.mjs", "*.html", "*.css", "*.lua", "*.ts", "*.tsx", "*.sass"}
+	--	'prettier/vim-prettier',
+	--	pattern = {"*.js", "*.jsx","*.mjs", "*.html", "*.css", "*.lua", "*.ts", "*.tsx", "*.sass"}
 	-- })
-	use({ 
+	use({
 		"iamcco/markdown-preview.nvim",
 		run = "cd app && npm install",
 		setup = function() vim.g.mkdp_filetypes = { "markdown" } end,
 		ft = { "markdown" }, })
-	use {
-		"folke/todo-comments.nvim",
-		requires = "nvim-lua/plenary.nvim",
-		config = function()
-			require("todo-comments").setup {
+		use {
+			"folke/todo-comments.nvim",
+			requires = "nvim-lua/plenary.nvim",
+			config = function()
+				require("todo-comments").setup {
 					-- your configuration comes here
 					-- or leave it empty to use the default settings
 					-- refer to the configuration section below
 				}
 			end
 		}
-	use("tomarrell/vim-npr")
-	-- markdown language support
-	use({
+		use("tomarrell/vim-npr")
+		-- markdown language support
+		use({
 			"iamcco/markdown-preview.nvim",
 			run = function() vim.fn["mkdp#util#install"]() end,
-	})
-	use({'jakewvincent/mkdnflow.nvim'})
+		})
+		use({'jakewvincent/mkdnflow.nvim'})
 
-	-- sessions
-  use "shatur/neovim-session-manager"
-	-- use {
-	-- 	'rmagatti/auto-session',
-	-- 	config = function()
-	-- 		require("auto-session").setup {
-	-- 			log_level = "error",
-	-- 			auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
-	-- 		}
-	-- 	end
-	-- }
-	use "arnamak/stay-centered.nvim"
-	use({
-		"nvim-telescope/telescope.nvim",
-		requires = "nvim-lua/plenary.nvim"
-	})
-	use({"aca/emmet-ls"})
-	use({"neovim/nvim-lspconfig"})
-	use "terrortylor/nvim-comment"
-	use('skywind3000/asyncrun.vim')
-	-- html and css plugins
-	use("ray-x/web-tools.nvim")
-	use("norcalli/nvim-colorizer.lua")
-end,
-config = {
-  display = {
-    open_fn = function()
-			return require('packer.util').float({ border = 'single' })
-    end
-  }
-}}
-)
+		-- sessions
+		use "shatur/neovim-session-manager"
+		-- use {
+		--	'rmagatti/auto-session',
+		--	config = function()
+		--		require("auto-session").setup {
+		--			log_level = "error",
+		--			auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
+		--		}
+		--	end
+		-- }
+		use "arnamak/stay-centered.nvim"
+		use({
+			"nvim-telescope/telescope.nvim",
+			requires = {
+				"nvim-telescope/telescope-live-grep-args.nvim",
+				"nvim-lua/plenary.nvim"
+            },
+			config = function ()
+				require("telescope").load_extension("live_grep.args")
+			end
+		})
+		use({"aca/emmet-ls"})
+		use({"neovim/nvim-lspconfig"})
+		use "terrortylor/nvim-comment"
+		use('skywind3000/asyncrun.vim')
+		-- html and css plugins
+		use("ray-x/web-tools.nvim")
+		use("norcalli/nvim-colorizer.lua")
+	end,
+	config = {
+		display = {
+			open_fn = function()
+				return require('packer.util').float({ border = 'single' })
+			end
+		}
+	}}
+	)
